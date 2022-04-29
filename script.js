e63f460161ba3f9c746895c55e50e8347439f2b8
schadi faour <schadi.faour@powercoders.org>
2022-04-29 16:51:43 +0200

    incomplete layout on the homepage and filled the empty links

--- script.js
+++ script.js
@@ -18,39 +18,25 @@ function closeMenu() {
   navMenu.classList.remove("active");
 }
 
-// --------------------------------------------------------------------------------------------------------------------------------
+// for the buttons on cards
 
-// for the blog page for the buttons to switch from new to old to all
-filterSelection("all");
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
+// const button = document.querySelector(".breakout-button");
 
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+// if (button) {
+//   button.addEventListener("click", (evt) => {
+//     evt.preventDefault();
 
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
+//     alert("Oh hi there 👋");
+//   });
+// }
+
+// the alert box when pressing the send button
+document.getElementById("save").onclick = function () {
+  myFunction();
+};
+function myFunction() {
+  document.getElementById("save");
+  alert(
+    "Error!! Please use the email schadi.faour@powercoders.org not this form! Thank you"
+  );
 }
